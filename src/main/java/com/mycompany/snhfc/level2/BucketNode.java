d3ec02d61e9763136a86882cc7bfdc5db305882e
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-04-19 15:20:01 +0200

    Document level details.

--- src/main/java/com/mycompany/snhfc/level2/BucketNode.java
+++ src/main/java/com/mycompany/snhfc/level2/BucketNode.java
@@ -1,27 +1,77 @@
 package com.mycompany.snhfc.level2;
 
 import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.query.N1qlQuery;
+import com.couchbase.client.java.query.N1qlQueryResult;
 import com.mycompany.snhfc.level3.DocumentChildFactory;
+import java.awt.Image;
 import java.beans.IntrospectionException;
+import java.util.List;
 import javax.swing.Action;
 import org.netbeans.api.annotations.common.StaticResource;
 import org.openide.nodes.BeanNode;
 import org.openide.nodes.Children;
+import org.openide.util.ImageUtilities;
+import org.openide.util.Utilities;
+import org.openide.util.lookup.AbstractLookup;
+import org.openide.util.lookup.InstanceContent;
 
 public class BucketNode extends BeanNode {
 
+    private static final String BASE_ICON_PATH = "com/mycompany/snhfc/level2/";
     @StaticResource
-    private static final String ICON = "com/mycompany/snhfc/level2/icon.png";
+    private static final String ICON = BASE_ICON_PATH + "icon.png";
+    @StaticResource
+    private static final String INDEXED = BASE_ICON_PATH + "index_icon.png";
+    @StaticResource
+    private static final String UNINDEXED = BASE_ICON_PATH + "unindexed_icon.png";
 
-    public BucketNode(Bucket bucket) throws IntrospectionException {
-        super(bucket, Children.create(new DocumentChildFactory(bucket), true));
+    public BucketNode(Bucket bean) throws IntrospectionException {
+        this(bean, new InstanceContent());
+    }
+
+    public BucketNode(Bucket bucket, InstanceContent ic) throws IntrospectionException {
+        super(bucket,
+              Children.create(new DocumentChildFactory(bucket), true),
+              new AbstractLookup(ic)
+        );
+        ic.add(bucket);
+        ic.add(this);
         setDisplayName(bucket.name());
         setIconBaseWithExtension(ICON);
     }
 
+    public void refresh() {
+        fireIconChange();
+        fireOpenedIconChange();
+    }
+
+     @Override
+    public Image getIcon(int type) {
+        return getCurrentImage();
+    }
+
+    private Image getCurrentImage() {
+        Bucket bucket = getLookup().lookup(Bucket.class);
+        Image original = ImageUtilities.loadImage(ICON);
+        N1qlQueryResult query = bucket.query(N1qlQuery.simple(String.format("select * from system:indexes where keyspace_id = '" + bucket.name() + "'", bucket.name())));
+        int size = query.allRows().size();
+        if (size > 0) {
+            return ImageUtilities.mergeImages(original, ImageUtilities.loadImage(INDEXED), 7, 7);
+        } else {
+            return ImageUtilities.mergeImages(original, ImageUtilities.loadImage(UNINDEXED), 7, 7);
+        }
+    }
+
+    @Override
+    public Image getOpenedIcon(int type) {
+        return getCurrentImage();
+    }
+
     @Override
     public Action[] getActions(boolean context) {
-        return new Action[]{};
+        List<? extends Action> bucketActions = Utilities.actionsForPath("Actions/Bucket");
+        return bucketActions.toArray(new Action[bucketActions.size()]);
     }
 
     @Override
---
+++ src/main/java/com/mycompany/snhfc/level2/CreatePrimaryIndexAction.java
@@ -0,0 +1,74 @@
+package com.mycompany.snhfc.level2;
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.query.N1qlQuery;
+import com.couchbase.client.java.query.N1qlQueryResult;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import org.netbeans.api.progress.BaseProgressUtils;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionRegistration;
+import org.openide.awt.DynamicMenuContent;
+import org.openide.util.ContextAwareAction;
+import org.openide.util.Lookup;
+import org.openide.util.NbBundle.Messages;
+import org.openide.util.RequestProcessor;
+import org.openide.util.Utilities;
+
+@ActionID(
+        category = "Bucket",
+        id = "org.netbeans.modules.couchbase.bucket.CreatePrimaryIndexAction"
+)
+@ActionRegistration(
+        lazy = false,
+        asynchronous = true,
+        displayName = "#CTL_CreatePrimaryIndexAction"
+)
+@Messages("CTL_CreatePrimaryIndexAction=Create Primary Index")
+public final class CreatePrimaryIndexAction extends AbstractAction implements ContextAwareAction {
+
+    private final Bucket bucket;
+    private final BucketNode bucketNode;
+
+    public CreatePrimaryIndexAction() {
+        this(Utilities.actionsGlobalContext());
+    }
+
+    public CreatePrimaryIndexAction(Lookup context) {
+        super(Bundle.CTL_CreatePrimaryIndexAction());
+        this.bucket = context.lookup(Bucket.class);
+        this.bucketNode = context.lookup(BucketNode.class);
+        N1qlQueryResult query = bucket.query(N1qlQuery.simple(String.format("select * from system:indexes where keyspace_id = '" + bucket.name() + "'", bucket.name())));
+        int size = query.allRows().size();
+        if (size > 0) {
+            setEnabled(false);
+        } else {
+            setEnabled(true);
+        }
+        putValue(DynamicMenuContent.HIDE_WHEN_DISABLED, true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ev) {
+        RequestProcessor.getDefault().post(new Runnable() {
+            @Override
+            public void run() {
+                final String bucketName = bucket.name();
+                BaseProgressUtils.showProgressDialogAndRun(new Runnable() {
+                    @Override
+                    public void run() {
+                        bucket.query(N1qlQuery.simple(String.format("create primary index on `" + bucketName + "`", bucketName)));
+                        bucketNode.refresh();
+                    }
+                }, "Indexing bucket '" + bucketName + "'...");
+            }
+        });
+    }
+
+    @Override
+    public Action createContextAwareInstance(Lookup context) {
+        return new CreatePrimaryIndexAction(context);
+    }
+
+}
